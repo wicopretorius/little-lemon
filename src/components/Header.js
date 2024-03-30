d69d20ff534c48f1b7339d61c2b2a12c9952cb92
wicopretorius <wico.pretorius@gmail.com>
2024-03-30 07:17:28 +0200

    add semantic tags

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <a href="homepage.html">
+            <img src="little_lemon_logo.png" alt="Company Logo"/>
+        </a>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul>  Navagation links
+			<li><a href="index.html">Home</a></li>
+			<li><a href="location.html">Location</a></li>
+			<li><a href="blog.html">Blog</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
